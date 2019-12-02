9680ec27a8e04086f9cd7d51528ce6eb649b508e
Jing Xie <xiejing418@gmail.com>
2019-12-02 15:24:08 -0800

    update

--- GetTweets.js
+++ GetTweets.js
@@ -3,13 +3,12 @@ var config = require('./config') //this is we import the config file which is a 
 var T = new Twit(config); //this is the object of twit which will help us to call functions inside it
 var params = {
 screenName: 'realDonaldTrump',
-count: 1
 } // this is the param variable which will have key and value ,the key is the keyword which we are interested in searching and count is the count of it
-T.get('statuses/user_timeline', {screen_name: params.screenName,tweet_mode:'extended'},searchedData); // get is the function to search the tweet which three paramaters 'search/tweets',params and a callback function.
+T.get('statuses/user_timeline', {screen_name: params.screenName,tweet_mode:'extended', count:30 },searchedData); // get is the function to search the tweet which three paramaters 'search/tweets',params and a callback function.
 function searchedData(err, data, response) {
 //console.log(data);
 data.forEach(element => {
-    if(element.user.id === 25073877 && element.in_reply_to_status_id === null && (element.retweeted === false ||element.retweeted == undefined)){
+    if(element.in_reply_to_status_id === null && (element.retweeted === false ||element.retweeted == undefined)){
         console.log("The  twit is: "+ JSON.stringify(element. full_text));
     }
     
